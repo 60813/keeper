72b0f74593ffe2eee7f83315ed06b2a7d58ffc1b
huangdou <huangdou@.hikvision.com>
2016-12-12 10:34:47 +0800

    add some test unit

--- src/test/java/com/keeper/expansion/TestClientLock.java
+++ src/test/java/com/keeper/expansion/TestClientLock.java
@@ -1,10 +1,13 @@
 package com.keeper.expansion;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.keeper.client.KeeperClient;
+import com.keeper.client.exception.KeeperException;
 import com.keeper.expansion.locks.KeeperLock;
 import com.keeper.expansion.locks.KeeperMutexLock;
 
@@ -28,124 +31,223 @@ public class TestClientLock {
 			client = null;
 		}
 	}
+	
+	class MyThread extends Thread{
+		private KeeperLock lock ;
+		
+		public MyThread(KeeperLock lock,String name) {
+			super();
+			this.lock = lock;
+			this.setName(name);
+		}
+
+		@Override
+		public void run() {
+			lock.lock();
+			try {
+				System.out.println(Thread.currentThread().getName()+"  locked ");
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+			}finally{
+				System.out.println(Thread.currentThread().getName()+"  unlock ");
+				lock.unlock();
+			}
+		}
+		
+	}
 	@Test
-	public void testLock() {
+	public void testLock() throws InterruptedException {
 		final KeeperLock lock1 = new KeeperMutexLock("testlocka", client);
 		final KeeperLock lock2 = new KeeperMutexLock("testlocka2", client);
+		
+		new MyThread(lock1,"lock1a").start();
+		new MyThread(lock2,"lock2222222222a").start();
+		new MyThread(lock1,"lock1b").start();
+		
+		new MyThread(lock2,"lock2222222222b").start();
+		new MyThread(lock2,"lock2222222222c").start();
+		new MyThread(lock2,"lock2222222222d").start();
+		new MyThread(lock1,"lock1c").start();
+		
+		Thread.sleep(100000);
+	}
+	
+	
+	//Use 3 machines,2 threads per machine to perform simultaneous addition from 1 to 100,result 600
+	@Test
+	public void machine1() throws InterruptedException {
+		final AtomicBoolean down1 = new AtomicBoolean();
+		final AtomicBoolean down2 = new AtomicBoolean();
+		final String additionPath = "/addtitionData5";
+		final KeeperClient client = new KeeperClient("127.0.0.1:2181");
+		final KeeperLock lock = new KeeperMutexLock("addtition", client);
+		final int total = 100;
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
-				final KeeperLock lock = new KeeperMutexLock("testlocka", client);
-				lock.lock();
-				System.out.println("locked 11");
-				try {
-					Thread.sleep(10000);
-					
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}finally{
-					lock.unlock();
+				int i = 0 ;
+				while (i<total){
+					i++;
+					lock.lock();
+					try{
+						byte[] data = client.read(additionPath);
+						
+						int ret = Integer.parseInt(new String(data)) + 1;
+						client.update(additionPath, (ret+"").getBytes());
+					}catch(KeeperException e){
+						System.out.println(e);
+					}finally{
+						lock.unlock();
+					}
 				}
+				down1.compareAndSet(false, true);
 			}
 		}).start();
 		new Thread(new Runnable() {
-			
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
-				final KeeperLock lock = new KeeperMutexLock("testlocka", client);
-				lock.lock();
-				System.out.println("locked 22");
-				try {
-					Thread.sleep(10000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}finally{
-					lock.unlock();
+				int i = 0 ;
+				while (i<total){
+					i++;
+					lock.lock();
+					try{
+						byte[] data = client.read(additionPath);
+						
+						int ret = Integer.parseInt(new String(data)) + 1;
+						client.update(additionPath, (ret+"").getBytes());
+					}catch(KeeperException e){
+						System.out.println(e);
+					}finally{
+						lock.unlock();
+					}
 				}
-			}
-		}).start();
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				lock1.lock();
-				System.out.println("locked 33");
-				try {
-					Thread.sleep(10000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}finally{
-					lock1.unlock();
-				}
+				down2.compareAndSet(false, true);
 			}
 		}).start();
 		
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				final KeeperLock lock = new KeeperMutexLock("testlocka2", client);
-				lock.lock();
-				System.out.println("locked 111");
-				try {
-					Thread.sleep(10000);
-					
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}finally{
-					lock.unlock();
-				}
-			}
-		}).start();
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				final KeeperLock lock = new KeeperMutexLock("testlocka2", client);
-				lock.lock();
-				System.out.println("locked 222");
-				try {
-					Thread.sleep(10000);
-					
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}finally{
-					lock.unlock();
-				}
-			}
-		}).start();
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				lock2.lock();
-				System.out.println("locked 333");
-				try {
-					Thread.sleep(10000);
-					
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}finally{
-					lock2.unlock();
-				}
-			}
-		}).start();
-		try {
-			Thread.sleep(60000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		while(!down1.get()){
+			
+		}
+		while(!down2.get()){
+			
 		}
 	}
 	
-
+	@Test
+	public void machine2() throws InterruptedException {
+		final AtomicBoolean down1 = new AtomicBoolean();
+		final AtomicBoolean down2 = new AtomicBoolean();
+		final String additionPath = "/addtitionData5";
+		final KeeperClient client = new KeeperClient("127.0.0.1:2181");
+		final KeeperLock lock = new KeeperMutexLock("addtition", client);
+		final int total = 100;
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 0 ;
+				while (i<total){
+					i++;
+					lock.lock();
+					try{
+						byte[] data = client.read(additionPath);
+						
+						int ret = Integer.parseInt(new String(data)) + 1;
+						client.update(additionPath, (ret+"").getBytes());
+					}catch(KeeperException e){
+						System.out.println(e);
+					}finally{
+						lock.unlock();
+					}
+				}
+				down1.compareAndSet(false, true);
+			}
+		}).start();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 0 ;
+				while (i<total){
+					i++;
+					lock.lock();
+					try{
+						byte[] data = client.read(additionPath);
+						
+						int ret = Integer.parseInt(new String(data)) + 1;
+						client.update(additionPath, (ret+"").getBytes());
+					}catch(KeeperException e){
+						System.out.println(e);
+					}finally{
+						lock.unlock();
+					}
+				}
+				down2.compareAndSet(false, true);
+			}
+		}).start();
+		
+		while(!down1.get()){
+			
+		}
+		while(!down2.get()){
+			
+		}
+	}
+	
+	@Test
+	public void machine3() throws InterruptedException {
+		final AtomicBoolean down1 = new AtomicBoolean();
+		final AtomicBoolean down2 = new AtomicBoolean();
+		final String additionPath = "/addtitionData5";
+		final KeeperClient client = new KeeperClient("127.0.0.1:2181");
+		final KeeperLock lock = new KeeperMutexLock("addtition", client);
+		final int total = 100;
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 0 ;
+				while (i<total){
+					i++;
+					lock.lock();
+					try{
+						byte[] data = client.read(additionPath);
+						
+						int ret = Integer.parseInt(new String(data)) + 1;
+						client.update(additionPath, (ret+"").getBytes());
+					}catch(KeeperException e){
+						System.out.println(e);
+					}finally{
+						lock.unlock();
+					}
+				}
+				down1.compareAndSet(false, true);
+			}
+		}).start();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 0 ;
+				while (i<total){
+					i++;
+					lock.lock();
+					try{
+						byte[] data = client.read(additionPath);
+						
+						int ret = Integer.parseInt(new String(data)) + 1;
+						client.update(additionPath, (ret+"").getBytes());
+					}catch(KeeperException e){
+						System.out.println(e);
+					}finally{
+						lock.unlock();
+					}
+				}
+				down2.compareAndSet(false, true);
+			}
+		}).start();
+		
+		while(!down1.get()){
+			
+		}
+		while(!down2.get()){
+			
+		}
+	}
 }
